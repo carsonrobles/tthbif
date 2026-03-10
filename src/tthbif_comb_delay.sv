3662ee58a64c4caade6902b10fd411f6435f5bdc
Carson Robles <carson.robles@gmail.com>
2026-03-09 22:55:43 -0700

    Inv

--- src/tthbif_comb_delay.sv
+++ src/tthbif_comb_delay.sv
@@ -7,7 +7,6 @@ module tthbif_comb_delay (
       // give some delay per "buffer"
       assign #1 z_o = a_i;
 `else
-  `ifdef SKY130_INV
       wire inv;
 
       sky130_fd_sc_hd__inv_1 u_inv0_dont_touch (
@@ -19,12 +18,6 @@ module tthbif_comb_delay (
         .A ( inv ),
         .Y ( z_o )
       );
-  `else
-      sky130_fd_sc_hd__dlygate4sd2 u_dlygate_dont_touch (
-        .A ( a_i ),
-        .X ( z_o )
-      );
-  `endif
 `endif
 
 endmodule
